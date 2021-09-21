b51d78cb51f535cbfcb398f7f84a438d3c5d1ebb
Jiawei He <45532786+JiaweiHe98@users.noreply.github.com>
2021-09-21 17:36:02 -0400

    Fetch create-react-app

---
+++ simple-react-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+
+  pairs = [
+    {
+      id: 1,
+      key: "Something here!",
+      value: "Some other things here!"
+    }
+    {
+      id: 2,
+      key: "Something here!",
+      value: "Some other things here!"
+    }
+  ]
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ simple-react-app/src/components/Items/ItemList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+const ItemList = (props) => {
+    let content = "";
+
+    <Item></Item>
+
+
+    return content
+}
